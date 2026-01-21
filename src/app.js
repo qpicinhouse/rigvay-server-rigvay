bfb8436d72aa1723499bebf950779e4dc8bd5cce
Aman Kumar <amankumar8675@gmail.com>
2026-01-22 02:23:11 +0530

    app rate limt

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ const app = express();
 
 // Global middlewares
 app.use(helmet());
+// app.set("trust proxy", 1);
 app.use(cors({
   origin: [
     "http://43.205.229.172",
@@ -30,7 +31,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(rateLimiter);
+// app.use(rateLimiter);
+
 
 // Routes
 app.use("/api/auth", authRoutes);
