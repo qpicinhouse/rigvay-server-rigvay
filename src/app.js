8fba0900567f6ac80ce4d3dd9e2299b39733057b
Aman Kumar <amankumar8675@gmail.com>
2026-01-24 15:02:51 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ app.use(cors({
     "http://localhost:5173",
     'http://localhost:3000',
     'http://localhost:3001',
+    "https://rigvay-admin.vercel.app",
     "https://rigvay.com"
   ],
   credentials: true,
